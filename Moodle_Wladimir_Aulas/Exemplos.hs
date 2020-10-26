56d6edf0894b6fc4478d8700c77c3e5fc89bf753
willsaraiva <guilhermewillian.ti@gmail.com>
2020-10-26 19:13:35 -0300

    Atividades Realizadas

---
+++ Moodle_Wladimir_Aulas/Exemplos.hs
@@ -0,0 +1,16 @@
+-- Nós pegamos um número. Se ele for par, dividimos por dois.
+--  Se for ímpar, dividimos ele por 3 e somamos com mais 1. 
+--  Pegamos o número resultante e aplicamos a mesma coisa nele,
+--  produzindo um novo número e assim por diante.
+
+-- Questão: para todos os números iniciais entre 1 e 100, quantas sequências tem com um tamanho maior do que 15
+
+chain :: (Integral a) => a -> [a]  
+chain 1 = [1]  
+chain n  
+    | even n =  n:chain (n `div` 2)  
+    | odd n  =  n:chain (n*3 + 1)
+
+numLongChains :: Int  
+numLongChains = length (filter isLong (map chain [1..100]))  
+    where isLong xs = length xs > 15
